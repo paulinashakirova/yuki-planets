0fefa06f34719cbaabbe032ccecec38445c11008
Paulina Shakirova <paulinashakirova@gmail.com>
2024-04-24 14:58:20 +0200

    refactor page logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
-  import PlanetsList from '@/components/PlanetsList.vue'
-  import { usePlanetApiStore } from '@/stores/planets'
-  import type { Planet } from '@/types/planet'
   import { computed } from 'vue'
 
+  import type { Planet } from '@/types/planet'
+
+  import PlanetsList from '@/components/PlanetsList.vue'
+  import { usePlanetApiStore } from '@/stores/planets'
+
   const store = usePlanetApiStore()
-  const planets = computed<Planet[]>(() => store.data) //removing .results for fake data
-  const isLoading = computed(() => store.loading)
-  const error = computed(() => store.error) // Access the error state
-  // onBeforeMount(async () => {
-  //   await store.fetchPlanets()
-  // })
+  const planets = computed<Planet[]>(() => store.data)
 </script>
 
 <template>
   <main class="mx-auto my-16 max-w-7xl">
-    <h1 class="text-center text-2xl mb-6 uppercase">
+    <h1 class="text-center text-2xl mb-6 uppercase mx-4">
       Here you can see the list of all planets
     </h1>
-    <h2 class="text-center text-xl mb-12">
+    <h2 class="text-center text-xl mb-12 mx-4">
       Click on a tile to get more details
     </h2>
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-4">
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-4">
       <PlanetsList :all-planets="planets" />
     </div>
   </main>
--- src/views/PlanetView.vue
+++ src/views/PlanetView.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
-  import SinglePlanet from '@/components/SinglePlanet.vue'
-  import { usePlanetApiStore } from '@/stores/planets'
+  import { computed, ref } from 'vue'
+  import { useRouter } from 'vue-router'
+
   import type { Planet } from '@/types/planet'
-  import { computed } from 'vue'
-  import { useRoute } from 'vue-router'
 
-  const route = useRoute()
+  import PlanetCard from '@/components/PlanetCard.vue'
+  import { usePlanetApiStore } from '@/stores/planets'
+
+  const router = useRouter()
   const store = usePlanetApiStore()
-  const planets = computed<Planet[]>(() => store.data) //.results
+  const planets = ref<Planet[]>(store.data)
 
-  const currentPlanet = computed(() =>
-    planets.value?.find(planet => planet.name === route.params.name)
+  const currentPlanet = computed<any>(() =>
+    planets.value?.find(
+      planet => planet.name === router.currentRoute.value.params.name
+    )
   )
 </script>
+
 <template>
   <div class="flex flex-col justify-center items-center mt-16">
     <div
-      class="flex flex-col p-4 border-2 border-teal-400 rounded-md m-4 border-dashed shadow-xl shadow-teal-50">
-      <SinglePlanet :planet="currentPlanet" />
+      class="flex flex-col p-4 border-2 border-teal-400 rounded-md m-4 border-dashed shadow-xl shadow-teal-50 max-w-96 min-h-64 items-stretch justify-around">
+      <div v-if="currentPlanet">
+        <PlanetCard :planet="currentPlanet">
+          <template #heading>{{ currentPlanet?.name }}</template>
+        </PlanetCard>
+      </div>
+      <div v-if="!currentPlanet">
+        <h2 class="font-semibold text-xl text-center">
+          Unfortunately, there is no such planet. You can see the list of all
+          available planets on the main page.
+        </h2>
+      </div>
       <button
         @click="$router.push({ path: '/' })"
         class="border rounded-lg py-1 px-3 m-4 border-teal-400 transition delay-75 duration-100 ease-in-out hover:-translate-y-[2px] hover:scale-105">
